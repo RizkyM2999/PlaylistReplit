173dd5997e1acacd5a6f56c0363d722fd898b895
RizkyM2999 <44426590+RizkyM2999@users.noreply.github.com>
2024-03-09 08:44:09 +0000

    Change key

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ data_sebelum = input("Masukkan data sebelum replace: ")
 data_setelah = input("Masukkan data yang ingin digunakan setelah replace: ")
 
 # Baca isi file .m3u
-with open('cubmu.txt', 'r') as file:
+with open('playlist.m3u', 'r') as file:
     data = file.read()
 
 # Ganti teks yang diinginkan
 data = data.replace(data_sebelum, data_setelah)
 
 # Tulis perubahan ke file yang sama
-with open('cubmu.txt', 'w') as file:
+with open('playlist.m3u', 'w') as file:
     file.write(data)
